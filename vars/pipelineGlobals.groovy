a793d4b5405cc5ab1b6ace2d56bcd551084b5c21
Harika Eravathri <harikaeravathri@gmail.com>
2024-11-28 15:34:54 -0800

    jenkins

--- vars/pipelineGlobals.groovy
+++ vars/pipelineGlobals.groovy
@@ -1,16 +1,16 @@
-// def getAccountID(String environment){
-//     switch(environment) { 
-//         case 'dev': 
-//             return "905418172435"
-//         case 'qa':
-//             return "905418172435"
-//         case 'uat':
-//             return "905418172435"
-//         case 'pre-prod':
-//             return "905418172435"
-//         case 'prod':
-//             return "905418172435"
-//         default:
-//             return "nothing"
-//     } 
-// }
+def getAccountID(String environment){
+    switch(environment) { 
+        case 'dev': 
+            return "905418172435"
+        case 'qa':
+            return "905418172435"
+        case 'uat':
+            return "905418172435"
+        case 'pre-prod':
+            return "905418172435"
+        case 'prod':
+            return "905418172435"
+        default:
+            return "nothing"
+    } 
+}
